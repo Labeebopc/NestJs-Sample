aa4b379769fd30601b7d5ccbb172cba65cc56bfa
Labeeb OPC <Labeebopc313@gmail.com>
2024-09-26 20:22:27 +0400

    CRUD operations completed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { BookService } from './app.service';
 import { Book } from './fakeDatabase';
 
 @Controller('books')
 export class BooksController {
-  constructor(private readonly booksService: BookService) {}
+  constructor(private readonly booksService: BookService) { }
 
   @Get()
   getAllBooks(): Book[] {
@@ -12,8 +12,33 @@ export class BooksController {
   }
 
   @Get('getById/:id')
-  getBookById(@Param('id') id: string) : Book | undefined {
+  getBookById(@Param('id') id: string): Book | undefined {
     const bookID = +id
     return this.booksService.findById(bookID)
   }
+
+  @Post()
+  addBook(@Body() book: Partial<Book>): Book | undefined {
+    const bookData = book
+
+    if (!book.author || !book.title || !book.publicationYear) return undefined
+
+    return this.booksService.create(bookData)
+  }
+
+  @Put(':id')
+  updateBook(
+    @Param('id') id: string,
+    @Body() book: Partial<Book>)
+    : Book | undefined {
+    return this.booksService.update(+id, book)
+  }
+
+  @Delete(':id')
+  deleteBook(
+    @Param('id') id: string): Book[] {
+
+    return this.booksService.delete(+id)
+  }
+
 }
